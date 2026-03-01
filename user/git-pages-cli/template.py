d0a3dc20d5ec0d107a6bdbc5e5993ce49d229e35
Gnarwhal <git.aspect893@passmail.net>
2026-03-01 19:25:12 +0000

    user/git-pages-cli: new package

---
+++ user/git-pages-cli/template.py
@@ -0,0 +1,12 @@
+pkgname = "git-pages-cli"
+pkgver = "1.10.0"
+pkgrel = 0
+build_style = "go"
+hostmakedepends = ["go"]
+pkgdesc = "Command-line application for publishing sites to git-pages"
+license = "0BSD"
+url = "https://codeberg.org/git-pages/git-pages-cli"
+source = (
+    f"https://codeberg.org/git-pages/git-pages-cli/archive/v{pkgver}.tar.gz"
+)
+sha256 = "0ac04e1dd03a3d3325c6332e28f392c8e89911b297aca44bffc1439f1c60a478"
